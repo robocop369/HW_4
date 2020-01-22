6ba6daea38749c35a9a9dd0f73c84eae40438e59
Jon <jcontreras39@gmail.com>
2020-01-21 18:46:51 -0600

    click feature works

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var playButton = document.querySelector("#start");
 var secondsDisplay = document.querySelector("#timer");
-var qtitle = document.querySelector("h4");
+var qtitle = document.querySelector("#qtitle");
 var choices = document.querySelectorAll('.list-group-item')
 
 var answerButton = document.querySelector("#q-list-tab");
 
+var answerChoice0 = document.querySelector('[href="#list-Answer0"]');
+var answerChoice1 = document.querySelector('[href="#list-Answer1"]');
+var answerChoice2 = document.querySelector('[href="#list-Answer2"]');
+var answerChoice3 = document.querySelector('[href="#list-Answer3"]');
 
 var totalSeconds = 5;
 var secondsElapsed = 0;
@@ -12,6 +16,7 @@ var interval;
 var questioncount;
 
 //Timer
+
 function getFormattedSeconds() {
     var secondsLeft = (totalSeconds - secondsElapsed);
     console.log(secondsLeft);
@@ -49,7 +54,6 @@ function startTimer() {
     }, 1000);
 }
 
-
 function stopTimer() {
     secondsElapsed = 0;
     renderTime();
@@ -60,32 +64,52 @@ function stopTimer() {
 //Questions
 
 function getQuestions(n) {
-qtitle.textContent = allquestions[n]['title'];
+qtitle.textContent = allquestions[n].title;
 
 for (i=0; i<choices.length; i++) {
-   choices[i].textContent=allquestions[n]['choices'][i]
+   choices[i].textContent=allquestions[n].choices[i]
 }
 
 }
 
 
 //Result
+
 function showResult(){
- 
+ alert('this is working')
 }
 
-// function setTimePreferences() {
-//     localStorage.setItem(
-//         "preferences",
-//         JSON.stringify({
-//             workMinutes: workMinutesInput.value.trim(),
-//             restMinutes: restMinutesInput.value.trim()
-//         })
-//     );
 
-// }
+function submitAnswer() {
+   
+    // find out if click was correct
+    // find out if they were right?
+if 
+    
+    (choices[i]=this.allquestions[n].answer){
+    alert('correct')
+    } else{
+        alert('wrong')
+    }
 
+    // -- compare which one they clicked with which the index of the correct one from choices
+    // -- -- alert good job
+    // -- -- else alert get a job
+    // is this the last quesion 
+    // display final reslut
+    
+    // call getQuestions with new question
+}
 playButton.addEventListener("click", startTimer);
-answerButton.addEventListener("click", showResult);
-// stopButton.addEventListener("click", stopTimer);
-// statusToggle.addEventListener("change", toggleStatus);
+//answerButton.addEventListener("click", showResult);
+// loop thru choices
+for (let i = 0; i < choices.length; i++) {
+    // add an event listener to each choice
+    choices[i].addEventListener('click', submitAnswer)
+    
+}
+
+answerChoice0.addEventListener("click", submitAnswer);
+answerChoice1.addEventListener("click", submitAnswer);
+answerChoice2.addEventListener("click", submitAnswer);
+answerChoice3.addEventListener("click", submitAnswer);
